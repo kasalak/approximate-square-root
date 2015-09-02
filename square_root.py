ad33f7b9344992e73a87a58311ef31c26a37d0d7
Karthik Kasala <kasalak@gmail.com>
2015-09-01 23:18:32 -0400

    Update square_root.py

--- square_root.py
+++ square_root.py
@@ -1,1 +1,9 @@
+def sqrt_approx():
+    x = int(input("Give a positive number. "))
+    y = 1
+    while round((y**2 - x), 3) != 0:
+        y = (y+x/y)/2.0
+        
+    print("The square root of {} is {}".format(x,y))
+sqrt_approx()
 
