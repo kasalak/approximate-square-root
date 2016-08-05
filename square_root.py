34874684e53f839a5a2d308ce355d2be3c934c7a
Karthik <karthik.kasala@gmail.com>
2016-08-05 13:16:45 -0400

    did hard mode

--- square_root.py
+++ square_root.py
@@ -1,4 +1,22 @@
-# approximate-square-root
-A Python program to calculate square roots by using Newton's Method of Approximation
+#Newton's Approximation of square root
+def approx_square_root():
+    y = 1
+    x = 0
+    count = 0
+    try:
+        x = int(input("Give a positive number. "))
 
-Created for the Iron Yard Python class as part of Cohort V
+    except ValueError:
+        print("That is not a positive number. Try again.")
+        exit()
+    if x < 0:
+        print("That is not a positive number. Try again.")
+        exit()
+    else:
+        while round((y**2 - x), 3) != 0:
+            y = (y+x/y)/2.0
+            if y ** 2 != x:
+                count += 1
+                print("{} number of iterations have occurred, the new guess is {}".format(count, y))
+            print("The square root of {} is {}".format(x,round(y, 3)))
+approx_square_root()
